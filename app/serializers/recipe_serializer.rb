4b1de270c5d5cdcfbb7d2507ffe546134bc1259b
spensation <spensation@gmail.com>
2018-08-11 10:27:55 -0700

    fetch request working again

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,5 +1,5 @@
 class RecipeSerializer < ActiveModel::Serializer
-attributes :id, :title, :category, :serves, :directions, :prep_time, :cook_time, :total_time, :likes, :comments
+attributes :id, :title, :category, :serves, :directions, :prep_time, :cook_time, :total_time, :food_image, :likes, :comments
 has_many :ingredients, serializer: IngredientSerializer
 
 end
